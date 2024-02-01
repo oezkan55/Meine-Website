35211c17d02a2d16c3d85c6ad8729c56045e9d35
oezkan55 <alibeyoezkan@googlemail.com>
2024-02-01 15:49:02 +0100

    update

--- src/js/script.js
+++ src/js/script.js
@@ -8,7 +8,3 @@ const mobBtn = document.querySelector(".navigation__hamburger");
 mobBtn.addEventListener("click", () => {
   nav.classList.toggle("navigation--active");
 });
-
-const newFeature = function () {
-  console.log("Wilkommen zu der Applikation");
-};
